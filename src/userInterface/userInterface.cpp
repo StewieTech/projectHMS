ecbcaf936b43e87c7d7eb27ddf48f1e59d8d695c
flystewie <77213085+flystewie@users.noreply.github.com>
2024-02-24 10:58:34 -0500

    Update Appointment Schedule

--- src/userInterface/userInterface.cpp
+++ src/userInterface/userInterface.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
+#include<vector>
+#include<string>
+#include<list>
 
 using namespace std;
+
+/*
+Appointment Scheduling (Errol):
+   - Implement a class called "Appointment" to represent an appointment. 
+It should store the patient information, the medical staff assigned, the appointment time, and any necessary procedures.
+   - Utilize STL containers to handle appointment scheduling, track appointment conflicts, and manage appointment cancellations or rescheduling.
+*/
+
+
+// Appointment Scheduling
+class Appointment {
+    private:
+        string patient; // Will make a dynamic reference to the patient class from Question 1
+        string medicalStaff;
+        string appointmentTime;
+        string procedures;
+
+    public:
+        Appointment(string& patInput, const string& mInput, const string& atInput, const string& prInput) : patient(patInput), medicalStaff(mInput), appointmentTime(atInput), procedures(prInput) {}
+        
+        void displayMenu() const {
+            cout << "Patient: " << patient << endl;
+            cout << "Medical Staff: " << medicalStaff << endl;
+            cout << "Appointment Time: " << appointmentTime << endl;
+            cout << "Procedures: " << procedures << endl;
+        }
+};
+
+
+
+
+
+
+
+// User Interface
+/* 
+
+User Interface (Errol):
+   - Implement a user interface to interact with the Hospital Management System.
+   - Allow users to add patients, schedule appointments, manage medical staff, handle exceptions gracefully, and display relevant information.
+   - Provide appropriate menus, prompts, and error messages to enhance usability.
+
+*/
 
 void displayMenu() {
     cout << "HMS Menu Options" << endl;
@@ -11,4 +57,10 @@ void displayMenu() {
     cout << "0. Exit Menu" << endl; 
     cout << "Enter Your Choice" << endl;
 
+}
+
+// void userInput(::vector<Patient*>& patients, list<Appointment>& appointments)
+void userInput(list<Appointment>& appointments)
+{ 
+    int choice
 }
