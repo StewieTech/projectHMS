79363290f81e19dd40fa26e0f8efa6f8c3817be3
flystewie <77213085+flystewie@users.noreply.github.com>
2024-02-24 10:17:32 -0500

    Add userInterface.cpp

---
+++ src/userInterface/userInterface.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void displayMenu() {
+    cout << "HMS Menu Options" << endl;
+    cout << "1. Schedule Appointment" << endl; 
+    cout << "2. Cancel Appointment" << endl; 
+    cout << "3. Manage Staff" << endl; 
+    cout << "4. Find Patient" << endl; 
+    cout << "0. Exit Menu" << endl; 
+    cout << "Enter Your Choice" << endl;
+
+}
