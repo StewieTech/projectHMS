3e55db6e1a6a4c299ff1d9313e5a7def3ca893ac
Akdun <wusizhe991134@gmail.com>
2024-03-09 12:40:58 -0500

    Integrated patient.h and declared friendship in appointment

--- include/appointment.h
+++ include/appointment.h
@@ -2,10 +2,17 @@
 #define APPOINTMENT_H
 
 #include <string>
-#include "Patient.h"
+#include <time.h>
+#include "Patient.h"	// deadlocking declaration, pls review
 #include "MedicalStaff.h"
 
 using namespace std;
+
+// Forward declaration to avoid circular dependencies
+class Patient;
+class InPatient;
+class OutPatient;
+
 
 class Appointment {
 public:
@@ -22,9 +29,21 @@ public:
     void setPatient(Patient* patient);
     void setMedicalStaff(MedicalStaff* staff);
 
+    // Friend function declaration:
+    friend void Patient::setPatientAppointment(Patient* patient, Appointment* appointment);
+    friend void InPatient::setPatientAppointment(Patient* patient, Appointment* appointment);
+    friend void OutPatient::setPatientAppointment(Patient* patient, Appointment* appointment);
+
+    // Display apppointment history
+    friend void Patient::displayHistory();
+    friend void InPatient::displayHistory();
+    friend void OutPatient::displayHistory();
+
 private:
     string appointmentTime;
-    Patient* patient;       // Pointer to the patient associated with the appointment
+    Patient* patient;       // Pointer to the patient associated with the appointment - only to identify inpatient or outpatient
+    	InPatient* inPatient;	//will need to use template in this case
+    	OutPatient* outPatient;
     MedicalStaff* staff;    // Pointer to the medical staff associated with the appointment
 };
 
--- include/patient.h
+++ include/patient.h
@@ -3,29 +3,72 @@
 
 #include <string>
 #include <vector>
+#include <time.h>
 #include "Appointment.h" 
 
 using namespace std;
 
-class Inpatient;
-class Outpatient;
+class Appointment;
+class InPatient;
+class OutPatient;
 
 class Patient {
+	friend class Appointment; // allow access from Appointment objects;
 public:
-    Patient(string name, int age, char gender);
+    Patient(int ID, string name, int age, char gender); // need to set initial appointment here
     virtual ~Patient();
 
     virtual string displayInfo() const;
-    virtual int getId() const;  // Hypothetical function to retrieve ID
 
-    // Member functions 
-    void confirmInfo(int newAge, char newGender, string address, int phoneNum); //confirm patient info function
+    // Accessors - getters
+    int getId() const;  // Retrieve ID. No need to override
+	string getName() const;
+	int getAge() const;
+	char getGender() const;
+	string getInsurer() const;
+	string getInsuranceNum() const;
+	int getHp() const;
+	int getSan() const;
+	string getDept() const;
+
+	bool getUrgency() const;
+	bool getStatusInpatient() const;
+	bool getStatusOutpatient() const;
+
+	Appointment* getInitStep() const;
+	Appointment* getNextStep() const;
+	int getTotalInCents() const;
+
+	//Accessors - setters
+	Patient* setID(int i);
+	Patient* setName(string n);
+	Patient* setAge (int i);
+	Patient* setGender(char g);
+	Patient* setInsurer(string i);
+	Patient* setInsuranceNum(string i);
+	Patient* setHp(int h);
+	Patient* setSan(int s);
+	Patient* setDept(string d);
+
+	Patient* setUrgency();
+	Patient* resetUrgency();
+	Patient* setInpatient();
+	Patient* resetInpatient();
+	Patient* setOutpatient();
+	Patient* resetOutpatient();
+
+	Patient* setAppointment(Appointment* appointment);
+	Patient* setTotalInCents(int t);
+
+
+    // Member functions
+	Patient* preProcess();
+	void confirmInfo(int newAge, char newGender, string address, int phoneNum); //confirm patient info function
     void updateInfo(int newAge, char newGender, string address, int phoneNum); //update patient info function
-    void addMedicalProcedure(string procedureName); //add medical procedure function
-    string getHealthHistory() const; //function to get health history
-
-    // Function to set the appointment
-    void setAppointment(Appointment* appointment);
+    void addMedicalProcedure(string procedureName); //add medical procedure function - not sure if I will keep it
+    void displayHealthHistory () const;
+    Appointment* getHealthHistory() const; //function to get health history
+    void setAppointment(Appointment* appointment); // Function to set the appointment
 
 protected:
     string name;
@@ -35,26 +78,104 @@ protected:
     string insuranceNum;
     int id;  // Assuming each patient has a unique ID
     vector<string> medicalProcedures;  // Store a list of medical procedures
-    Appointment* appointment;  // Pointer to the patient's appointment
+
+    bool flag_urgency = false;	// Pls display urgent patients first in all caller functions
+
+	int hp;		// has range of 1-5
+	int san;	// has range of 1-5
+	string dept;	// range within defined departments
+
+	// Determined by pre-process
+	/* On the application level, when patient is classified, pls create new objects of derived class accordingly
+	 * We can use "static_cast<Derived*>(this)->derivedMethod();" to do so,
+	 * but creating a pointer to derived class might cause internal memory leak - pls remind me to write lesson learned
+	 */
+	bool is_inpatient = false;
+	bool is_outpatient = false;
+
+	/*
+	 * The list will be in-build, as:
+	 * Patient: reception -> appointment.pre-process -> classification into outpatient & inpatient;
+	 * 		Outpatient: appointment.testing & consultation (...-> more testing if needed) -> payment;
+	 * 		Inpatient: 	inpatient.room allocation (built in) -> appointment.treatment -> appointment.monitoring -> billing;
+	 */
+	Appointment* initialStep;
+	Appointment* nextStep;
+	int totalExpenseByCents;
+
 };
 
-class Inpatient : public Patient {
+class InPatient : public Patient {
 public:
-    Inpatient(string name, int age, char gender, int roomNumber, appointment * appointment);
+    InPatient(string name, int age, char gender, int roomNumber, appointment * appointment);
     virtual string displayInfo() const override;
 
 private:
     int roomNumber;
+
+    friend class Appointment; // allows access from Appointment objects
+public:
+	InPatient(int ID, string name, int age, char gender, int roomNumber, appointment * appointment));
+	virtual string displayInfo() const override;
+
+	// Accessors - getters
+	int getRoomNo() const;
+	time_t getInTime() const;
+	time_t getInTime() cosnt;
+
+	// Acceessors - setters
+	InPatient* setRoomNo(int r);
+	InPatient* setInTime(time_t it);
+	InPatient* setOutTime(time_t ot);
+
+	InPatient* setAppointment(Appointment* appointment);
+	InPatient* setTotalInCents(int t);
+
+	//other functions
+	InPatient* randomProcess();
+	InPatient* redoClassify();
+	InPatient* transferToOutPatient();
+	InPatient* transferOut(string nameHospital);
+	InPatient* exitCured();
+	// A function to gather history appointments and present them
+	void displayHistory();
+
+protected:
+	int roomNumber;
+	time_t inTime;
+	time_t outTime;
+
+	bool is_inpatient = true;
+	bool is_outpatient = false;
+
 };
 
-class Outpatient : public Patient {
+class OutPatient : public Patient {
+	friend class Appointment; // allows access from Appointment objects
 public:
-    Outpatient(string name, int age, char gender, string appointmentDate, string doctorName);
-    virtual string displayInfo() const override;
+	OutPatient(string name, int age, char gender, string appointmentDate, string doctorName);
+	virtual string displayInfo() const override;
 
-private:
-    string appointmentDate;
-    string doctorName;
+	//Accessors
+	//Accessors - getters
+	time_t getAppTime();
+	string getDocName();
+	//Accessors - setters
+	OutPatient* setAppTime(time_t at);
+	OutPatient* setDocName(string dn);
+
+	//Other func
+	OutPatient* randomProcess();
+	OutPatient* redoClassify();
+	OutPatient* transferToInPatient();
+	OutPatient* transferOut(string nameHospital);
+	OutPatient* exitCured();
+	// A function to gather history appointments and present them
+	void displayHistory();
+
+protected:
+	time_t appointmentTime;
+	string doctorName;
 };
 
 #endif // PATIENT_H
