416ba0b58ef9bd0e9312b9826c8380a82ff6d6d4
ocplusplus <160451189+ocplusplus@users.noreply.github.com>
2024-03-13 21:00:26 -0400

    Update medicalStaff.h

--- include/medicalStaff.h
+++ include/medicalStaff.h
@@ -1,6 +1,7 @@
 #ifndef MEDICALSTAFF_H
 #define MEDICALSTAFF_H
 #include <string>
+#include "exceptions.h"
 
 using namespace std;
 
@@ -45,4 +46,19 @@ public:
     static MedicalStaff* assignMedicalStaff(const string& specialization);
 };
 
+// Exception handling for invalid name
+class InvalidNameException : public BaseException {
+public:
+    void invalidNames(const std::string& Name) {
+        try {
+            for (char c : Name) {
+                if (!isalpha(c)) {
+                    throw std::invalid_argument("Names must contain letters only.");
+                }
+            }
+        } catch (const std::invalid_argument& e) {
+            reportError(e.what());
+        }
+    }
+};
 #endif // MEDICALSTAFF_H
