185a4813c8a8c74a4cf7349489d0d5003ac6fab7
ocplusplus <160451189+ocplusplus@users.noreply.github.com>
2024-03-17 23:48:50 -0400

    Update appointment.h

--- include/appointment.h
+++ include/appointment.h
@@ -1,89 +1,60 @@
-
 #ifndef APPOINTMENT_H
 #define APPOINTMENT_H
 
 #include <iostream>
 #include <string>
 #include <list>
-// #include "Patient.h"
-// #include "MedicalStaff.h"
-// #include "Procedure.h" 
-// #include "exceptions.h"
+#include "patient.h"
+#include "medicalStaff.h"
+#include "procedure.h" 
+#include "exceptions.h"
 
-// using namespace std;
+using namespace std;
 
-
-// Forward declaration to avoid circular dependencies
+// Forward declarations to avoid circular dependencies
 class Patient;
-class InPatient;
-class OutPatient;
 class MedicalStaff;
 class Procedure;
 
-// Exception class for appointment conflicts
-// redefinition of class
-// class AppointmentConflictException : public exception {
-// public:
-//     const char* what() const throw() {
-//         return "Appointment conflict detected.";
-//     }
-// };
-
-// Appointment Scheduling
 class Appointment {
 protected:
-    std::string appointmentTime;
+    string appointmentTime;
     Patient* patient;
-    InPatient* inPatient;
-    OutPatient* outPatient;
-    std::string procedures;
     MedicalStaff* medicalStaff;
-    // Procedure* procedures;
+    Procedure* procedure;
+    string procedures; // New member variable
 
 public:
     // Constructors
     Appointment();
-    Appointment(Patient* patInput, MedicalStaff* mInput, const std::string& atInput, const std::string& prInput);
-    // Appointment(const string& appointmentTime, Patient* patient, MedicalStaff* staff, Procedure* procedure);
+    Appointment(Patient* patInput, MedicalStaff* mInput, const string& atInput, const string& prInput); // Updated constructor
 
     // Accessors
-    std::string getAppointmentTime() const;
+    string getAppointmentTime() const;
     Patient* getPatient() const;
     MedicalStaff* getMedicalStaff() const;
-    Procedure* getProcedure() const; // Getter for procedure
+    Procedure* getProcedure() const;
+    string getProcedures() const; // Getter for procedures
 
     // Modifiers
-    
-    // void setAppointmentTime(const string& appointmentTime);
-    // void setPatient(Patient* patient);
-    // void setMedicalStaff(MedicalStaff* staff);
-    // void setProcedure(Procedure* procedure); // Setter for procedure
+    void setAppointmentTime(const string& appointmentTime);
+    void setPatient(Patient* patient);
+    void setMedicalStaff(MedicalStaff* staff);
+    void setProcedure(Procedure* procedure);
+    void setProcedures(const string& procedures); // Setter for procedures
 
-    // Current;y Patient does not have friend functions declared for the below
-
-
-    // Friend function declaration
-    // friend void Patient::setPatientAppointment(Patient* patient, Appointment* appointment);
-    // friend void InPatient::setPatientAppointment(Patient* patient, Appointment* appointment);
-    // friend void OutPatient::setPatientAppointment(Patient* patient, Appointment* appointment);
-
-    // Display appointment history
-    // friend void Patient::displayHistory();
-    // friend void InPatient::displayHistory();
-    // friend void OutPatient::displayHistory();
+    // Friend function declarations
+    friend void setPatientAppointment(Patient* patient, Appointment* appointment);
+    friend void displayHistory(const Patient& patient);
 
     // Additional functions for appointment scheduling
     void displayMenu() const;
-
-
-    // Procedure* procedure; // Need to update not sure where to get from Member variable representing the associated procedure
 };
 
 // Additional functions for appointment scheduling
-void appointmentSchedule(std::list<Appointment>& appointments, const Appointment& appointment);
-void appointmentCancel(std::list<Appointment>& appointments, const Appointment& appointment);
+void appointmentSchedule(list<Appointment>& appointments, const Appointment& appointment);
+void appointmentCancel(list<Appointment>& appointments, const Appointment& appointment);
 void displayMenu();
-void userInput(std::list<Appointment>& appointments, std::vector<Patient*>& patients, std::vector<MedicalStaff*>& staffMembers);
+void userInput(list<Appointment>& appointments, vector<Patient*>& patients, vector<MedicalStaff*>& staffMembers);
 
 #endif // APPOINTMENT_H
-
