2548beec029e6b0468669bbc198537b0fe5693e2
ocplusplus <160451189+ocplusplus@users.noreply.github.com>
2024-03-25 15:23:58 -0400

    Update appointment.h

--- FINAL/appointment.h
+++ FINAL/appointment.h
@@ -14,18 +14,21 @@ class Patient; // Forward declaration to avoid circular dependencies
 
 class Appointment {
 private:
-    string patient;
-    string patientID;
-    MedicalStaff* medicalStaff;
-    string appointmentTime;
-    string procedures;
-    bool canceled; // Flag to indicate whether the appointment is canceled
-    int appointmentID;
+    string patient; //name of patient
+    string patientID; //patient ID
+    MedicalStaff* medicalStaff; //pointer to medical staff assigned to appointment
+    string appointmentTime; //time of appointment
+    string procedures; //procedures associated with appointment
+    bool canceled; // Flag for if appointment is cancelled
+    int appointmentID; //appointment ID
 
 public:
+//Constructor
     Appointment(const string& patInput, int inputID, MedicalStaff* mInput, const string& atInput, const string& prInput);
-    ~Appointment();
+//Destructor    
+~Appointment();
 
+ //Getters
     string getPatientName() const;
     string getAppointmentTime() const;
     int getAppointmentID() const;
@@ -33,17 +36,25 @@ public:
     MedicalStaff* getMedicalStaff() const;
     string getProcedures() const;
 
+//Setters
     void setAppointmentTime(const string& appointmentTime);
     void setPatientID(const string& patientID);
     void setPatientName(const string& patientName);
     void setMedicalStaff(MedicalStaff* staff);
     void setProcedures(const string& procedures);
     void cancelAppointment();
+
+//Static function to reschedule appointment
     static void rescheduleAppointment(list<Appointment>& appointments, const string& appointmentID, const string& newTime);
 };
 
+//function to schedule appointment
 void appointmentSchedule(list<Appointment>& appointments, const Appointment& appointment);
+
+//Function to cancel appointment
 void cancelAppointment(list<Appointment>& appointments, const string& appointmentID);
+
+//function to cancel appointment (overload)
 void appointmentCancel(list<Appointment>& appointments, const Appointment& appointment);
 
 #endif // APPOINTMENT_H
